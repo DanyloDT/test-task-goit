51b83eb3b15f15ab24e68e97ca4b595e3aaa2445
Danylo Diachenko <danylodiachenko@gmail.com>
2023-08-02 23:02:06 +0300

    Правки HomePage,  опис сторінки

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,8 +7,7 @@ const HomePage = () => {
         <h1 className={css.title}>Welcome </h1>
         <p className={css.text}>
           The Tweets application is a simple client for viewing and managing
-          tweets. You can view a list of tweets, add new tweets, follow and
-          unfollow users.
+          tweets. You can view a list of tweets, follow and unfollow users.
         </p>
       </div>
     </div>
